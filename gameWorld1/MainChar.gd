dfc0479e832e557ee17a712e49211771d25cf04a
Wesley Nelson <wesslnelson@aol.com>
2017-05-25 23:43:31 -0400

    Changed char movement to only cardinal directions. Additionally, if one direction is pressed and then another (with first still held), will move in the second direction.

--- gameWorld1/MainChar.gd
+++ gameWorld1/MainChar.gd
@@ -3,12 +3,24 @@ extends KinematicBody2D
 var velocity = Vector2()
 var walkSpeed = 75
 var runSpeed = 100
-var direction = 0
+var direction = Vector2()
 
+# these are specifically defined for current sprite sheet
+# ie this is probably a bad way to do things/will be updated later
 var DOWN = 0
 var LEFT = 1
 var UP = 3
 var RIGHT = 2
+
+var NORTH = Vector2(0,-1)
+var SOUTH = Vector2(0,1)
+var WEST = Vector2(-1,0)
+var EAST = Vector2(1,0)
+
+var directionDict = { NORTH: UP, SOUTH: DOWN, WEST:LEFT, EAST: RIGHT }
+
+#used to keep track of which direction was most recently pressed
+var lastPressed = SOUTH
 
 
 var sprite
@@ -21,27 +33,37 @@ func _ready():
 	sprite = get_node("Sprite")
 
 func _input(event):
-	if(Input.is_action_pressed("ui_select")):
+	if(event.is_action_pressed("ui_select") and not event.is_echo()):
 		print(str(get_pos().x) +" "+str(get_pos().y))
+	
+	if(event.is_action_pressed("ui_down") and not event.is_echo()):
+		lastPressed = SOUTH
+	elif(event.is_action_pressed("ui_left") and not event.is_echo()):
+		lastPressed = WEST
+	elif(event.is_action_pressed("ui_right") and not event.is_echo()):
+		lastPressed = EAST
+	elif(event.is_action_pressed("ui_up") and not event.is_echo()):
+		lastPressed = NORTH
 	
 func _fixed_process(delta):
 	velocity = Vector2()
-	var moveSpeed = walkSpeed
-	#if(Input.is_action_just_pressed("ui_select")):
-	#	print(str(get_pos().x) +" "+str(get_pos().y))
 	
-	if(Input.is_action_pressed("ui_down")):
-		set_direction(DOWN)
-		velocity.y = moveSpeed
-	elif(Input.is_action_pressed("ui_up")):
-		set_direction(UP)
-		velocity.y = -moveSpeed
-	elif(Input.is_action_pressed("ui_left")):
-		set_direction(LEFT)
-		velocity.x = -moveSpeed
-	elif(Input.is_action_pressed("ui_right")):
-		set_direction(RIGHT)
-		velocity.x = moveSpeed
+	var numbPresses = numberOfDirectionsPressed()
+	if(numbPresses > 1):
+		set_direction(lastPressed)
+	else:
+		if(Input.is_action_pressed("ui_down")):
+			set_direction(SOUTH)
+		elif(Input.is_action_pressed("ui_up")):
+			set_direction(NORTH)
+		elif(Input.is_action_pressed("ui_left")):
+			set_direction(WEST)
+		elif(Input.is_action_pressed("ui_right")):
+			set_direction(EAST)
+	
+	#sets velocity only if some direction is currently pressed
+	if(numbPresses > 0):
+		set_velocity()
 	var motion = velocity * delta
 	move(motion)
 	
@@ -57,4 +79,14 @@ func _fixed_process(delta):
 
 func set_direction(dir):
 	direction = dir
-	sprite.set_frame(direction)
+	sprite.set_frame(directionDict[direction])
+
+func set_velocity():
+	velocity = direction * walkSpeed
+	
+func numberOfDirectionsPressed():
+	var num = 0
+	for dir in ["ui_down", "ui_up", "ui_left", "ui_right"]:
+		if(Input.is_action_pressed(dir)):
+			num +=1
+	return num
