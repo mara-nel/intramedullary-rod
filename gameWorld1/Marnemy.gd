474e853cb41f117c78ae9b4892e8de001af4691e
Wesley Nelson <wesslnelson@aol.com>
2017-05-28 15:50:00 -0400

    Added a general and death animation to marnemy

--- gameWorld1/Marnemy.gd
+++ gameWorld1/Marnemy.gd
@@ -12,13 +12,14 @@ var failedMoveAttempts = 0
 # set true if a navigation2d exists and you want to move around in it
 export (bool) var mobile = true
 
-
 var NORTH = Vector2(0,-1)
 var SOUTH = Vector2(0,1)
 var WEST = Vector2(-1,0)
 var EAST = Vector2(1,0)
 var DIRECTIONS = [ NORTH, SOUTH, WEST, EAST ]
 
+onready var animator = get_node("AnimationPlayer")
+onready var collider = get_node("CollisionPolygon2D")
 
 signal dead
 
@@ -27,54 +28,55 @@ func _ready():
 	set_fixed_process(true)
 	direction = DIRECTIONS[randi()%4]
 	directionChanged()
+	animator.play("Moving")
 
 	
 func _fixed_process(delta):
 	# this should all get cleanup some
-	
-	if(is_colliding()):
-		#print("enemy hit: "+get_collider().get_name())
-		var collWith = get_collider()
-		if(collWith.is_in_group("weapon")):
-			died()
+	if(!isDead):
+		if(is_colliding()):
+			#print("enemy hit: "+get_collider().get_name())
+			var collWith = get_collider()
+			if(collWith.is_in_group("weapon")):
+				died()
 			
-		elif(collWith.is_in_group("player")):
-			collWith.gotHitByEnemy()
+			elif(collWith.is_in_group("player")):
+				collWith.gotHitByEnemy()
 	
-	#follows a path
-	if(get_parent().get_type() == "PathFollow2D"):
-		get_parent().set_offset(get_parent().get_offset() + (walkSpeed*delta))
+		#follows a path
+		if(get_parent().get_type() == "PathFollow2D"):
+			get_parent().set_offset(get_parent().get_offset() + (walkSpeed*delta))
 	
-	#wanders around in the nav
-	elif(mobile):
-		if(nav == null):
-			#get the navigation2d which will be a child off of the root
-			# better hope the guy is a child of a child of whatever has the nav
-			#nav = get_parent().get_parent().get_node("Navigation2D")
+		#wanders around in the nav
+		elif(mobile):
+			if(nav == null):
+				#get the navigation2d which will be a child off of the root
+				# better hope the guy is a child of a child of whatever has the nav
+				#nav = get_parent().get_parent().get_node("Navigation2D")
 			
-			# if using the individual tile method, NEED TO UNCOMMENT THE ABOVE AND COMMENT OUT THE BELOW
-			# right now this always returns the first Nav tagged with enemyNav
-			nav = get_tree().get_nodes_in_group("enemyNav")[0]
+				# if using the individual tile method, NEED TO UNCOMMENT THE ABOVE AND COMMENT OUT THE BELOW
+				# right now this always returns the first Nav tagged with enemyNav
+				nav = get_tree().get_nodes_in_group("enemyNav")[0]
 			
-		if(nav != null): # fails when no such navigation2d node is found
-			# turns around if it hits something
-			if(is_colliding()):
-				direction = getNewDirection()
-			elif(OS.get_unix_time()- timeOfLastDirectionChange > minDirectionChangeTime):
-				direction = getNewDirection()
-			velocity = direction*walkSpeed*delta
+			if(nav != null): # fails when no such navigation2d node is found
+				# turns around if it hits something
+				if(is_colliding()):
+					direction = getNewDirection()
+				elif(OS.get_unix_time()- timeOfLastDirectionChange > minDirectionChangeTime):
+					direction = getNewDirection()
+				velocity = direction*walkSpeed*delta
 						
-			if(canMove()):
-				move(velocity)
-				failedMoveAttempts = 0
-			else:
-				failedMoveAttempts += 1
-				# a bad assumption, but if it cant move in any direction, then it was forced
-				# there, presumably by something that would have killed it (issue with hammer 
-				# doesnt report collision)
-				if(failedMoveAttempts >8 ):
-					died()
-				direction = getNewDirection()
+				if(canMove()):
+					move(velocity)
+					failedMoveAttempts = 0
+				else:
+					failedMoveAttempts += 1
+					# a bad assumption, but if it cant move in any direction, then it was forced
+					# there, presumably by something that would have killed it (issue with hammer 
+					# doesnt report collision)
+					if(failedMoveAttempts >8 ):
+						died()
+					direction = getNewDirection()
 	
 	
 # returns true if the moving in the current direction stays in the mesh
@@ -107,6 +109,14 @@ func randomGamePoint():
 
 # does the things that happen at death
 func died():
+	animator.play("Death")
 	isDead = true
-	emit_signal("dead")
+	collider.set_trigger(true)
+
 	
+func done_dying():
+	emit_signal("dead")
+
+
+func _on_AnimationPlayer_finished():
+	pass # replace with function body
