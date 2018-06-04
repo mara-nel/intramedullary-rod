9532b50951ad39ef5dc77ca1b3b20e824bbd20b8
wesslnelson <wesslnelson@gmail.com>
2018-06-04 19:06:53 -0400

    Can tell which game cell is clicked in and the coords of its center

---
+++ Godotorini/Main.gd
@@ -0,0 +1,33 @@
+extends Node
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+var player
+var Board
+var cell
+
+
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	player = get_node("Player1")
+	Board = get_node("Board")
+	#tiles = Board.get_tileset()
+	
+	
+
+func _process(delta):
+	# Called every frame. Delta is time since last frame.
+	# Update game logic here.
+	pass
+
+func _input(event):
+	if event is InputEventMouseButton:
+		#mouseClickPos = event.positiona
+		print("Mouse Click/Unclick at: ", event.position)
+		print(" which is in tile: ", Board.world_to_map(event.position))
+		print(" with center coord: ", Board.map_to_world(Board.world_to_map(event.position)))
+
--- Godotorini/Player.gd
+++ Godotorini/Player.gd
@@ -18,23 +18,24 @@ func _ready():
 	screensize = get_viewport_rect().size
 
 
-func move(dir):
-	pass
+func move(location):
+	position = location
 
 
 func _process(delta):
-	var velocity = Vector2() # the player's movement vector
-	if Input.is_action_pressed("ui_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("ui_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("ui_down"):
-		velocity.y += 1
-	if Input.is_action_pressed("ui_up"):
-		velocity.y -= 1
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * SPEED
-
-	position += velocity * delta
-	position.x = clamp(position.x, 0, screensize.x)
-	position.y = clamp(position.y, 0, screensize.y)
+#	var velocity = Vector2() # the player's movement vector
+#	if Input.is_action_pressed("ui_right"):
+#		velocity.x += 1
+#	if Input.is_action_pressed("ui_left"):
+#		velocity.x -= 1
+#	if Input.is_action_pressed("ui_down"):
+#		velocity.y += 1
+#	if Input.is_action_pressed("ui_up"):
+#		velocity.y -= 1
+#	if velocity.length() > 0:
+#		velocity = velocity.normalized() * SPEED
+#
+#	position += velocity * delta
+#	position.x = clamp(position.x, 0, screensize.x)
+#	position.y = clamp(position.y, 0, screensize.y)
+	pass
